085fd2f3710133cf90eb28e2040132017be5ea47
alirez asadi <ali.s.asadi98@gmail.com>
2020-10-25 13:11:10 +0330

    users app created

--- blog/settings.py
+++ blog/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'app_blog',
+    'app_blog.apps.AppBlogConfig',
+    'users.apps.UsersConfig',
 ]
 
 MIDDLEWARE = [
